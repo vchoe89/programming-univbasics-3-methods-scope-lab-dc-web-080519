48358c70c0689607ea70bf8fffca66bdc7d29514
Victor Jian Choe <vchoe89@gmail.com>
2019-07-22 19:45:30 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -10,3 +10,8 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  status = "It's Dangerous To Go Aline! Take This."
+  puts status
+end
